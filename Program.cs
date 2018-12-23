d5d29af78ccfb98891c78baf5e6890e0624e2094
Igor Dvorkin <idvorkin@gmail.com>
2018-12-23 07:11:49 -0800

    Initial checkin

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Google.Cloud.Language.V1;
+using System;
+
+namespace google_nlp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // The text to analyze.
+            string text = "Hello World!";
+            var client = LanguageServiceClient.Create();
+            var response = client.AnalyzeSentiment(new Document()
+            {
+                Content = text,
+                Type = Document.Types.Type.PlainText
+            });
+            var sentiment = response.DocumentSentiment;
+            Console.WriteLine($"Score: {sentiment.Score}");
+            Console.WriteLine($"Magnitude: {sentiment.Magnitude}");
+        }
+    }
+}
