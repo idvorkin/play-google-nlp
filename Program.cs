312eb28bc20844013c9e182d01c52cf5c426bd77
Igor Dvorkin <idvorkin@gmail.com>
2018-12-23 14:46:10 -0800

    Adding future support for watson ML (currently broken)
    Broken since can't seem to install into dotnet core 2.2

--- Program.cs
+++ Program.cs
@@ -1,25 +1,50 @@
 ﻿using Google.Cloud.Language.V1;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
+// using IBM.WatsonDeveloperCloud.NaturalLanguageUnderstanding.v1.Model;
+
 
 namespace google_nlp
 {
     class Program
     {
+        // Sharing credentials between Windows and Unix is a pain. Do it via C# for now.
+        static string homeDirectory = Environment.GetEnvironmentVariable("HOME");
         static void Main(string[] args)
         {
-            // Sharing credentials between Windows and Unix is a pain. Do it via C# for now.
-            var homeDirectory = Environment.GetEnvironmentVariable("HOME");
-            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", $"{homeDirectory}/gits/igor2/secrets/google-nlp-igorplaygocreds.json");
 
             var fileToAnalyze = $"{homeDirectory}/gits/igor2/750words/2018-12-04.md";
             Console.WriteLine($"Running NLP on {fileToAnalyze}");
 
-            var nlpClient = LanguageServiceClient.Create();
+            AnalyzeWithGoogle(fileToAnalyze);
+            AnalyzeWithWatson(fileToAnalyze);
+        }
+        private static void AnalyzeWithWatson(string fileToAnalyze)
+        {
+            var secrets = JObject.Parse(File.ReadAllText($"{homeDirectory}/gits/igor2/secretBox.json"));
+            var key = secrets["IBMWatsonKey"];
+            if (key == null) throw new InvalidDataException("Missing Key");
 
+            /*
+            var iamAssistantTokenOptions = new TokenOptions()
+            {
+                IamApiKey = "<iam-apikey>",
+                ServiceUrl = "<service-endpoint>"
+            };
+            */
+
+            //var nluClient = new NaturalLanguageUnderstandingService();
+            //var _assistant = new AssistantService(iamAssistantTokenOptions, "<version-date>");
+        }
+
+        private static void AnalyzeWithGoogle(string fileToAnalyze)
+        {
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", $"{homeDirectory}/gits/igor2/secrets/google-nlp-igorplaygocreds.json");
+            var nlpClient = LanguageServiceClient.Create();
             // The text to analyze.
             Document textToAnalyze = (new Document()
             {
