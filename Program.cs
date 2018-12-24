516bc43a7896038136ef1ba78590df3316e86e9c
Igor Dvorkin <idvorkin@gmail.com>
2018-12-24 07:13:50 -0800

    Adding support for command line (pass via dotnet run -- --help)

--- Program.cs
+++ Program.cs
@@ -5,8 +5,21 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
-// using IBM.WatsonDeveloperCloud.NaturalLanguageUnderstanding.v1.Model;
+using CommandLine;
 
+// using IBM.WatsonDeveloperCloud.NaturalLanguageUnderstanding.v1.Model;
+class Options
+{
+    const string USE_HARDCODED_FILE = "USE_HARDCODED_FILE";
+    [Option('f', "file",  Default=USE_HARDCODED_FILE, Required = false, HelpText = "Input files to be processed.")]
+    public string InputFile { get; set; }
+    [Option('s', "stdin", Default = false, Required = false, HelpText = "Process Standard in.")]
+    public bool StdIn { get; set; }
+    public bool IsUseHardcodedFile()
+    {
+        return this.InputFile == Options.USE_HARDCODED_FILE;
+    }
+}
 
 namespace google_nlp
 {
@@ -16,19 +29,35 @@ namespace google_nlp
         static string homeDirectory = Environment.GetEnvironmentVariable("HOME");
         static void Main(string[] args)
         {
+            var program = new Program();
+            Parser.Default.ParseArguments<Options>(args)
+              .WithParsed<Options>(opts =>
+              {
+                 var fileToAnalyze = opts.IsUseHardcodedFile() ?
+                      $"{homeDirectory}/gits/igor2/750words/2018-12-04.md" :
+                      opts.InputFile;
 
-            var fileToAnalyze = $"{homeDirectory}/gits/igor2/750words/2018-12-04.md";
-            Console.WriteLine($"Running NLP on {fileToAnalyze}");
-
-            AnalyzeWithGoogle(fileToAnalyze);
-            AnalyzeWithWatson(fileToAnalyze);
+                  Console.WriteLine($"Running NLP on {fileToAnalyze}");
+                  var textToAnalyze = File.ReadAllText(fileToAnalyze).ToLower();
+                  program.InstanceMain(textToAnalyze);
+              }
+              );
         }
-        private static void AnalyzeWithWatson(string fileToAnalyze)
+
+
+        void InstanceMain(string textToAnalyze)
+        {
+            AnalyzeWithGoogle(textToAnalyze);
+            AnalyzeWithWatson(textToAnalyze);
+        }
+
+        private void AnalyzeWithWatson(string textToAnalyze)
         {
             var secrets = JObject.Parse(File.ReadAllText($"{homeDirectory}/gits/igor2/secretBox.json"));
             var key = secrets["IBMWatsonKey"];
             if (key == null) throw new InvalidDataException("Missing Key");
 
+            // Can't import watson libraries - excited for when can do that.
             /*
             var iamAssistantTokenOptions = new TokenOptions()
             {
@@ -41,29 +70,29 @@ namespace google_nlp
             //var _assistant = new AssistantService(iamAssistantTokenOptions, "<version-date>");
         }
 
-        private static void AnalyzeWithGoogle(string fileToAnalyze)
+        private void AnalyzeWithGoogle(string textToAnalyze)
         {
             Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", $"{homeDirectory}/gits/igor2/secrets/google-nlp-igorplaygocreds.json");
             var nlpClient = LanguageServiceClient.Create();
             // The text to analyze.
-            Document textToAnalyze = (new Document()
+            Document docToAnalyze = (new Document()
             {
                 Type = Document.Types.Type.PlainText,
-                Content = File.ReadAllText(fileToAnalyze).ToLower(),
+                Content = textToAnalyze
             });
 
-            var rEntities = nlpClient.AnalyzeEntitySentiment(textToAnalyze);
+            var rEntities = nlpClient.AnalyzeEntitySentiment(docToAnalyze);
             foreach (var entity in rEntities.Entities.ToList().OrderBy(e => e.Salience))
             {
                 Console.WriteLine($"{entity.Name} I:{entity.Salience} M:{entity.Sentiment.Magnitude} S:{entity.Sentiment.Score} T:{entity.Type}");
             }
-            var rClassify = nlpClient.ClassifyText(textToAnalyze);
+            var rClassify = nlpClient.ClassifyText(docToAnalyze);
             foreach (var category in rClassify.Categories.OrderBy(_ => _.Confidence))
             {
                 Console.WriteLine($"{category.Name} C:{category.Confidence}");
             }
 
-            var rSentiment = nlpClient.AnalyzeSentiment(textToAnalyze);
+            var rSentiment = nlpClient.AnalyzeSentiment(docToAnalyze);
             var interestingSentances = rSentiment.Sentences.
                     Where(_ => !_.Text.Content.StartsWith("#")). // Remove markdown headers.
                     Where(_ => _.Sentiment.Magnitude != 0). // Remove things that don't have magnitude.
---
+++ f
@@ -0,0 +1,121 @@
+Running NLP on /home/idvorkin/gits/igor2/750words/2018-11-04.md
+promo M:0 S:0
+value M:1.1 S:-0.5
+rating M:0.1 S:0
+words M:0 S:0
+Stock M:0 S:0
+question M:0.9 S:0.9
+bluetooth M:1.7 S:0.4
+other M:0 S:0
+choice M:0 S:0
+dive M:0 S:0
+Vlad M:0.4 S:-0.2
+idea M:0.7 S:0.7
+prioritizing M:0 S:0
+company M:0 S:0
+pay M:0 S:0
+trick M:1.6 S:-0.8
+need M:0.1 S:0
+stuff M:0 S:0
+question M:0 S:0
+ROI M:0 S:0
+Sandy M:0 S:0
+job M:0.9 S:0.9
+cubbys M:0 S:0
+specialization M:0.1 S:-0.1
+question M:0 S:0
+pride M:0 S:0
+matter M:0 S:0
+target M:0 S:0
+grand M:0 S:0
+top M:0 S:0
+monitor size M:0 S:0
+fac M:0.1 S:0.1
+Tori M:0.1 S:0
+bit M:0 S:0
+laptop - yup M:0 S:0
+question M:0.3 S:0.3
+yup M:0 S:0
+point M:0.8 S:-0.8
+each other M:0 S:0
+story M:0 S:0
+wy M:0 S:0
+trick M:0 S:0
+soem M:0.1 S:0
+prose mode M:0 S:0
+lineup M:0.9 S:0.9
+Ill M:0.5 S:0.5
+other M:0 S:0
+cable - yup M:0.9 S:0.9
+story M:0.8 S:0.8
+coins M:0 S:0
+coins M:0 S:0
+trick M:0.8 S:-0.8
+coins M:0 S:0
+nothing M:0 S:0
+magic M:0 S:0
+desk M:0 S:0
+stuff M:0.1 S:-0.1
+magic M:0 S:0
+respect M:0 S:0
+playlists M:0 S:0
+device M:0.1 S:0.1
+og M:0 S:0
+shit M:0.1 S:-0.1
+reason M:0 S:0
+folks M:0 S:0
+group M:0 S:0
+snags M:0.1 S:-0.1
+tonne M:0.1 S:0.1
+side M:0 S:0
+lactures M:0 S:0
+question si M:0 S:0
+bar M:0 S:0
+floor M:0 S:0
+pI M:0 S:0
+interview M:0 S:0
+words M:0 S:0
+technique M:0.9 S:0.4
+both M:0 S:0
+Ha M:0 S:0
+desks M:0.2 S:-0.2
+shoes M:0.1 S:-0.1
+pair M:0.3 S:-0.3
+hat M:0 S:0
+noew M:0 S:0
+realdy M:0 S:0
+multple M:0 S:0
+crap M:0.5 S:-0.5
+VS M:0.6 S:0.6
+pRasanna M:0 S:0
+wto M:0 S:0
+Yup M:0 S:0
+otherw ise M:0 S:0
+progress M:0 S:0
+points M:0.1 S:0.1
+hundrerd burkcs M:0 S:0
+Gym habit M:0 S:0
+fun M:0.1 S:0.1
+bedroom M:0.1 S:-0.1
+Affirmations M:0 S:0
+undersatnd M:0.2 S:0.2
+Lets M:0 S:0
+food M:0.9 S:0.9
+clothes M:0 S:0
+nothing M:0.1 S:0.1
+bag M:0 S:0
+carry bag M:0 S:0
+keyboard M:0 S:0
+ecause M:0.9 S:0.9
+folds M:0.3 S:0.3
+kind M:0.3 S:0.3
+bags M:0 S:0
+plan M:0 S:0
+work M:0 S:0
+way M:0.1 S:0.1
+back M:0.1 S:-0.1
+## Grateful M:0 S:0
+Greg M:0 S:0
+Essential M:0.3 S:0.3
+difference M:0 S:0
+Context. 1 M:0 S:0
